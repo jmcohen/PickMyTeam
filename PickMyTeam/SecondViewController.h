edd945e2020bb2efcab3e384b50215ef14b45e41
Jeremy Cohen <jmcohen27@gmail.com>
2012-07-04 10:21:50 -0400

    Initial Commit

---
+++ PickMyTeam/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PickMyTeam
+//
+//  Created by Jeremy Cohen on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
